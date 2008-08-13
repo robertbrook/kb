dc14aef6f44d64372a27e0f3cc4790995e84d464
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-13 15:08:59 +0100

    show full notes in show view

--- spec/views/records/show.haml_spec.rb
+++ spec/views/records/show.haml_spec.rb
@@ -2,9 +2,18 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "/records/show.haml" do
   include RecordsHelper
-  
+
   before do
-    @record = mock_model(Record)
+    @record = mock_model(Record,
+        :title=>'Exit',
+        :suffix=>'Exit',
+        :initial=>'E',
+        :note=>'Example',
+        :web_page=>'url',
+        :first_name=>'first',
+        :middle_name=>'middle',
+        :last_name=>'last',
+        :summary_attributes=>{'web_page'=>'url'})
 
     assigns[:record] = @record
   end
