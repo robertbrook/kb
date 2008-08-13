a74b53fbb80e019fa4f71cb0801046cc9953f2c3
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-13 16:26:30 +0100

    formatting of note paragraphs and links in show view

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -3,4 +3,12 @@ module RecordsHelper
   def link_to_web_page(record)
     record.web_page.blank? ? '' : link_to(record.web_page[0..30]+'...', record.web_page)
   end
+
+  def format_notes(record)
+    formatted = record.note.strip
+    formatted.gsub!(/(http:\/\/\S+)/, '<a href="\1">\1</a>')
+    formatted.gsub!("\r\n\r\n","</p><p>")
+    formatted.gsub!("\r\n",'<br />')
+    "<p>#{formatted}</p>"
+  end
 end
--- spec/helpers/records_helper_spec.rb
+++ spec/helpers/records_helper_spec.rb
@@ -1,4 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe RecordsHelper do # Helper methods can be called directly in the examples (it blocks)
+describe RecordsHelper do
+
+  describe 'when asked to format notes' do
+    it 'should add line brake element for single line break' do
+      record = mock(Record, :note => "One\r\nTwo")
+      format_notes(record).should == '<p>One<br />Two</p>'
+    end
+    it 'should put paragraphs in paragraph elements' do
+      record = mock(Record, :note => "One\r\n\r\nTwo")
+      format_notes(record).should == '<p>One</p><p>Two</p>'
+
+      record = mock(Record, :note => "One\r\n\r\nTwo\r\n")
+      format_notes(record).should == '<p>One</p><p>Two</p>'
+    end
+    it 'should turn URIs in to hyperlinks' do
+      record = mock(Record, :note => "A http://host/path\r\nwebsite")
+      format_notes(record).should == '<p>A <a href="http://host/path">http://host/path</a><br />website</p>'
+    end
+  end
 end
