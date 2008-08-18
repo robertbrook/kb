bbfb1ff175305bed69118f059714540b0d5ddb17
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-18 11:15:36 +0100

    added in_place_editing as a git submodule - requires: git submodule update

--- spec/helpers/records_helper_spec.rb
+++ spec/helpers/records_helper_spec.rb
@@ -11,15 +11,20 @@ describe RecordsHelper do
       check_formatted "One\r\nTwo", '<p>One<br />Two</p>'
       check_formatted "One\nTwo", '<p>One<br />Two</p>'
     end
+
     it 'should put paragraphs in paragraph elements' do
       check_formatted "One\r\n\r\nTwo", '<p>One</p><p>Two</p>'
       check_formatted "One\r\n\r\nTwo\r\n", '<p>One</p><p>Two</p>'
       check_formatted "One\n\nTwo", '<p>One</p><p>Two</p>'
       check_formatted "One\n\nTwo\n", '<p>One</p><p>Two</p>'
     end
+
     it 'should turn URIs in to hyperlinks' do
-      check_formatted "A http://host/path\r\nwebsite", '<p>A <a href="http://host/path">http://host/path</a><br />website</p>'
-      check_formatted "A http://host/path\nwebsite", '<p>A <a href="http://host/path">http://host/path</a><br />website</p>'
+      check_formatted "A http://host/path\r\nwebsite",
+        '<p>A <a href="http://host/path">http://host/path</a><br />website</p>'
+
+      check_formatted "A http://host/path\nwebsite",
+        '<p>A <a href="http://host/path">http://host/path</a><br />website</p>'
     end
   end
 end
