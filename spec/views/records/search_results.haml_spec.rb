7a232580ed6896f6de1fd6d2e6f86c55f5c594ca
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-20 16:33:51 +0100

    show record name as a link to record in notes summary

--- spec/views/records/search_results.haml_spec.rb
+++ spec/views/records/search_results.haml_spec.rb
@@ -6,10 +6,35 @@ describe "/records/search_results.haml" do
 
   before do
     @template = 'records/search_results.haml'
-    @searched_for = 'term'
+    @searched_for = 'Group'
     assigns[:term] = @searched_for
   end
 
   it_should_behave_like "renders search form"
 
+  def do_render
+    render @template
+  end
+
+  describe 'when there are results' do
+    before do
+      @name = 'All Party Group'
+      @notes_summary = 'The all party group...'
+      @record = mock(Record, :name => @name, :notes_summary => @notes_summary, :id=>123)
+      records = [@record]
+      assigns[:records] = records
+    end
+    it 'should show result name in a heading' do
+      do_render
+      response.should have_tag("h4[class=name]", @name)
+    end
+    it 'should show result name as a link' do
+      do_render
+      response.should have_tag("a[href=#{record_path(@record)}]", @name)
+    end
+    it 'should show result notes summary' do
+      do_render
+      response.should have_tag("div[class=notes]", @notes_summary)
+    end
+  end
 end
