6339d3abaf669a1aaccf0ad4882e026b8c842e2e
robmckinnon <rob@theyworkforyou.co.nz>
2008-08-29 12:26:19 +0100

    added acts-as-taggable-on to config.gem ; requires rake gems:install

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "fastercsv"
+  config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
