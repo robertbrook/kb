7f71c190bec91a7ef4f3b4bf128e2560f77df0b1
Robert Brook <mail@robertbrook.com>
2008-09-01 16:01:08 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080829131042) do
+ActiveRecord::Schema.define(:version => 20080901112131) do
 
   create_table "records", :force => true do |t|
     t.string   "company"
