27b657c1b255c55084576b9651a22d5c7b6db08c
robmckinnon <rob@theyworkforyou.co.nz>
2008-09-01 17:26:45 +0100

    better identification of subject tags

--- db/migrate/20080829131042_populate_topic_tags.rb
+++ db/migrate/20080829131042_populate_topic_tags.rb
@@ -5,23 +5,13 @@ class PopulateTopicTags < ActiveRecord::Migration
       [:title, :first_name, :middle_name, :last_name, :suffix].each do |topic_attribute|
         topic = record.send(topic_attribute)
         unless topic.blank?
-          topic = topic.strip.downcase.gsub(/[^a-z^-^ ]/,'').squeeze(' ').strip
+          topic = topic.strip.downcase.strip.gsub(/:^$/,', ').gsub('/',', ').gsub(/[^a-z^-^ ^,]/,'').squeeze(' ').strip
           unless topic.blank? || topic == '-'
             topics << topic
           end
         end
       end
       topics = topics.join(', ')
-
-      topics.sub!('prime, ministers question, time', 'prime minister, question time')
-      topics.sub!('prime, ministers question time',  'prime minister, question time')
-      topics.sub!('prime, ministers ',               'prime minister, ')
-      topics.sub!('prime, ministers, ',              'prime minister, ')
-      topics.sub!('prime, minister and ',            'prime minister, ')
-      topics.sub!('prime, minister, ',               'prime minister, ')
-      topics.sub!('prime, ministerial record, ',     'prime minister, record ')
-      topics.sub!('prime, minister royal',           'prime minister, royal')
-      topics.sub!(/prime, ministers?$/,              'prime minister')
 
       topics.sub!('select, committees,', 'select committees, committees,')
       topics.sub!('select, committees ', 'select committees, committees, ')
@@ -48,12 +38,48 @@ class PopulateTopicTags < ActiveRecord::Migration
       topics.sub!('visiting each others','visiting')
       topics.sub!('govt', 'government')
       topics.sub!('defeats ', 'defeats, ')
-
+      topics.sub!('to contact ministersgovernment','contact, ministers, government')
+      topics.sub!('depts','departments')
+      topics.sub!(' grand, committee', ', grand committee, committee')
+      topics.sub!('ii house of commons evacuation','house of commons, evacuation')
+      topics.sub!('ii end europe','europe')
+      topics.sub!('rt hon sir','rt hon, sir')
+      topics.sub!('kg obe mp','kg, obe, mp')
+      topics.sub!('house of commons ','house of commons, ')
+      topics.sub!('wales representation in the','wales, representation')
+      topics.sub!('palace of westminster union','palace of westminster, union')
+      topics.sub!('in parliament','parliament')
+      topics.sub!('groups ','groups, ')
+      topics.sub!(' of ',', ')
+      topics.sub!('scottish','scotland, scottish')
+      topics.sub!('welsh','wales welsh')
+      topics.sub!('northern, ireland','northern ireland, ireland')
+      topics.sub!(', for ',', ')
+      topics.sub!(', ireland ',', ireland, ')
+      topics.sub!(', in, ',',')
+      topics.sub!(', inc, ',',')
+      topics.sub!('the, ','')
+      topics.sub!('elections ','elections, ')
+      topics.sub!('big, ben ','big ben, ')
+      topics.sub!('privy, council','privy council, council')
+      topics.sub!('privy, council','privy council, council')
+      topics.sub!(', by, ',', ')
+      topics.sub!(' to, ',', ')
+      topics.sub!(', to ',', ')
+      topics.sub!('prime, ministers question, time', 'prime minister, question time')
+      topics.sub!('prime, ministers question time',  'prime minister, question time')
+      topics.sub!('prime, ministers ',               'prime minister, ')
+      topics.sub!('prime, ministers, ',              'prime minister, ')
+      topics.sub!('prime, minister and ',            'prime minister, ')
+      topics.sub!('prime, minister, ',               'prime minister, ')
+      topics.sub!('prime, ministerial record, ',     'prime minister, record ')
+      topics.sub!('prime, minister royal',           'prime minister, royal')
+      topics.sub!(/prime, ministers?$/,              'prime minister')
 
       record.topic_list = topics
+      puts topics
 
       record.save!
-      puts record.topic_list.inspect
     end
   end
 
